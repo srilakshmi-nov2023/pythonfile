bd7b29a7fafa1315fbf6804768df4f59f757654e
srilakshmi <143934364+srilakshmi-2425@users.noreply.github.com>
2024-01-30 16:03:25 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,3 +2,4 @@ print("Hello python world")
 print("updted the code and push into devlopment branch")
 print("again i have some thing write in this code")
 print("here i am update in remote repository")
+print("update the code in remote repo also"

86db377f2b5882e40dd94c58ffeb8acc8fd49765
srilakshmi-2425 <srilakshmikintali2015@gmail.com>
2024-01-30 16:01:35 +0530

    update

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 print("Hello python world")
 print("updted the code and push into devlopment branch")
 print("again i have some thing write in this code")
+print("here we are write for stash command")
+print("updated")
 
